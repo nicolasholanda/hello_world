bafdf8fae371a32466b1b7ad3f78ec92814c0075
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-08-24 16:57:39 -0300

    Implementação do método hello_world

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-# TODO: Implementar o código do método!
 def hello_world():
     """
         Método responsável por exibir 'Hello World' no console.
     """
-    pass
+    print("Hello World!")
 
 
 if __name__ == "__main__":
