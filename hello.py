a3485f690010b5bf888d64ad0adb5a63ba47f775
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-08-24 16:56:28 -0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+# TODO: Implementar o código do método!
+def hello_world():
+    """
+        Método responsável por exibir 'Hello World' no console.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    hello_world()
