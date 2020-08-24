10b82e1f5a18af56dc7156c9c7291986e9ed2003
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-08-24 17:05:27 -0300

    Adicionando texto de bom dia com o nome do usuário

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import getpass
+
 
 def hello_world():
     """
         Método responsável por exibir 'Hello World' no console.
+        E exibir um bom dia com o o nome do usuário.
     """
-    print("Hello World!")
+    print(f"Hello World! Good morning, {getpass.getuser()}.")
 
 
 if __name__ == "__main__":
